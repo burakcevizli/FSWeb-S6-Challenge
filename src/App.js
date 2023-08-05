8fd55983045c873de01a031485c8e2adef9a5898
KrieqqeR <burakcevizli@gmail.com>
2023-08-06 00:12:48 +0300

    Buton hatası düzeltildi

--- src/App.js
+++ src/App.js
@@ -6,30 +6,34 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [karakterlerListesi , setKarakterlerListesi] = useState([])
+  const [karakterlerListesi, setKarakterlerListesi] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
- useEffect(()=>{
-  axios
-  .get("https://swapi.dev/api/people/")
-  .then((response)=>{
-    console.log(response.data)
-    setKarakterlerListesi(response.data)
-  })
-  .catch((error)=>{
-    console.log("Error is :",error)
-  })
- },[])
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => {
+        console.log(response.data)
+        setKarakterlerListesi(response.data)
+      })
+      .catch((error) => {
+        console.log("Error is :", error)
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">REACT WARS </h1>
       <h1 className='Ok'>↓</h1>
 
-      <Karakter karakterlerListesi = {karakterlerListesi}/>
+      {karakterlerListesi.map((karakter, index) => {
+        return <Karakter key = {index} karakter={karakter} />
+      })}
+
+
 
     </div>
   );
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -2,83 +2,50 @@
 
 import React, { useEffect, useState } from 'react';
 
-import axios from 'axios';
 
 const Karakter = (props) => {
-    const { karakterlerListesi } = props;
+    const { karakter } = props;
     const [dataGoster, setDataGoster] = useState(false)
-    const [episodeGoster, setEpisodeGoster] = useState(false)
-    const [episodes, setEpisodes] = useState([])
+
 
 
     const dataFonksiyon = () => {
         setDataGoster(!dataGoster)
     }
 
-    const episodeGosterFonksiyon = () => {
-        setEpisodeGoster(!episodeGoster)
-    }
+
+    return (<div className='Karakter-main-div'>
+        <button className='Button' onClick={dataFonksiyon}>
+
+            <p>{karakter.name}</p> <p className='Birth-year'>{karakter.birth_year}</p>
+
+
+            <div className='DataDetay'>
+
+                {dataGoster ? (
+                    <ul>
+                        <li>Gender : {karakter.gender}</li>
+                        <li>Height : {karakter.height}</li>
+                        <li>Mass : {karakter.mass}</li>
+                        <li>Eye Color : {karakter.eye_color}</li>
+                        <li>Hair-Color : {karakter.hair_color}</li>
+                        <li>Skin-Color : {karakter.skin_color}</li>
+                        
+                    </ul>
+                ) : null}
 
 
 
 
 
-    const karakterler = karakterlerListesi.map((karakter) => (
-        <div className='Karakter-main-div'>
-            <button className='Button' onClick={dataFonksiyon}>
+            </div>
 
-                <p>{karakter.name}</p> <p className='Birth-year'>{karakter.birth_year}</p>
-
-
-                <div className='DataDetay'>
-
-                    {dataGoster ? (
-                        <ul>
-                            <li>Gender : {karakter.gender}</li>
-                            <li>Height : {karakter.height}</li>
-                            <li>Mass : {karakter.mass}</li>
-                            <li>Eye Color : {karakter.eye_color}</li>
-                            <li>Hair-Color : {karakter.hair_color}</li>
-                            <li>Skin-Color : {karakter.skin_color}</li>
-                            
-                        </ul>
-                    ) : null}
+        </button>
+    </div>)
 
 
 
 
-
-                </div>
-
-            </button>
-        </div>
-    ));
-
-
-
-
-    useEffect(() => {
-        axios
-            .get("https://swapi.dev/api/films/1/")
-            .then((response) => {
-                console.log(response.data)
-                setEpisodes(response.data.results)
-            })
-            .catch((error) => {
-                console.log("Error : ", error)
-            }, [])
-    })
-
-
-
-
-    return (<div>
-
-        {karakterler}
-
-    </div>
-
-    )
 };
 
 export default Karakter;
